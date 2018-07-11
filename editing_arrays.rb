7eaa3decd1824016d15402a6b20d7d2355d9699d
01Divergent <iphone.claudia@icloud.com>
2018-07-11 15:18:42 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -13,6 +13,9 @@ end
 
 def add_colors
   rainbow_colors = []
+  rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
+  rainbow_colors[2] = "light_yellow"
   rainbow_colors.push("green")
   rainbow_colors.push("blue")
 end
